9113c04e08bfa9bafd708a5fa31800dae5a9c836
sneha-gowda <snehagowda9899@gmail.com>
2021-11-28 18:44:16 +0530

    assignment-completed

--- assignments/run_callback/run_callback.js
+++ assignments/run_callback/run_callback.js
@@ -1,4 +1,7 @@
 function RunCallback(a, b, cb) {
+    a=parseInt(a);
+    b=parseInt(b);
+    return cb(a+b);
     // Write you code here, you need to add a and b, pass the result into callback function cb as argument return the result
 }
 
